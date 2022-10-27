6b8c114861319f17f36a6149d455bf5c81d3c034
Ali Vosoughi <64918363+ali-vosoughi@users.noreply.github.com>
2022-10-26 22:00:06 -0400

    save

--- code/scripts/train_grd.sh
+++ code/scripts/train_grd.sh
@@ -40,8 +40,8 @@ OPTS+="--audLen 65535 " # 65535
 OPTS+="--audRate 11025 " #11025
 
 # learning params
-OPTS+="--num_gpus 4 "
-OPTS+="--workers 40 "
+OPTS+="--num_gpus 3 "
+OPTS+="--workers 12 "
 OPTS+="--batch_size_per_gpu 12 "
 OPTS+="--lr_sound_ground 1e-4 "
 OPTS+="--lr_frame_ground 1e-4 "
