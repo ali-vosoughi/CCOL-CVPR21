d53ddfec0537bf6caa26ef0f1e33893f12a8dd51
Ali Vosoughi <64918363+ali-vosoughi@users.noreply.github.com>
2022-10-28 16:56:17 -0400

    save adds

--- code/scripts/train_grd.sh
+++ code/scripts/train_grd.sh
@@ -2,8 +2,8 @@
 
 OPTS=""
 OPTS+="--id MUSIC_GRD "
-OPTS+="--list_train /localdisk2/dat/sdeng10/MUSIC/train.csv "
-OPTS+="--list_val /localdisk2/dat/sdeng10/MUSIC/val.csv "
+OPTS+="--list_train ../data/Music/train.csv "
+OPTS+="--list_val ../data/Music/val.csv "
 
 # Models
 OPTS+="--arch_sound_ground vggish "
